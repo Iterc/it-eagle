e135c16026ed476767cb1a05ffdad4d1d8a5e7c4
Calvin <98326529+Iterc@users.noreply.github.com>
2022-12-01 22:29:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+do {
+	document.querySelector('#contact-form').addEventListener('submit', (e) => {
+	    e.preventDefault();
+	    e.target.elements.name.value = '';
+	    e.target.elements.email.value = '';
+	    e.target.elements.message.value = '';
+	  });
+	
+	  function myFunction(){
+	    alert(document.getElementById("form-control").value);
+	    }
+} while (condition);
